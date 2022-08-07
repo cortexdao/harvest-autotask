0c1ab53e69efb007dab9e9c289aa8d7388b88b14
opz <will.shahda@gmail.com>
2022-08-06 22:17:28 -0400

    Switch to safe-core-sdk
    There was too much dependency on the tx service in safe-ethers-adapters

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const {
   DefenderRelaySigner,
   DefenderRelayProvider
 } = require('defender-relay-client/lib/ethers');
+const { default: Safe, EthersAdapter } = require("@gnosis.pm/safe-core-sdk");
 const {
   LP_SAFE_ADDRESS,
   LP_ACCOUNT_ADDRESS,
@@ -27,19 +28,53 @@ exports.getClaimNames = (zapNames, lpBalances) => {
   return claimNames;
 }
 
-exports.claim = async (signer) => {
+exports.getSafe = async (signer) => {
+  const ethAdapter = new EthersAdapter({ ethers, signer })
+  const safe = await Safe.create({ ethAdapter, safeAddress: LP_SAFE_ADDRESS })
+
+  return safe;
+}
+
+exports.createClaimTx = async (signer) => {
   const lpAccount = new ethers.Contract(LP_ACCOUNT_ADDRESS, LP_ACCOUNT_ABI, signer);
 
+  // Get the zaps to claim from
   const zapNames = await lpAccount.zapNames();
   const lpBalances = await exports.getLpBalances(lpAccount, zapNames);
   const claimNames = exports.getClaimNames(zapNames, lpBalances);
 
-  const tx = await lpAccount.claim(claimNames);
+  // Create the `claim` tx object
+  const data = lpAccount.interface.encodeFunctionData("claim", [claimNames]);
+  const tx = {
+    to: lpAccount.address,
+    data,
+    value: 0,
+  };
 
   return tx;
 }
 
 exports.main = async (signer) => {
+  let tx = await exports.createClaimTx(signer);
+
+  // Create Safe tx
+  const safe = await exports.getSafe(signer);
+
+  const safeTx = await safe.createTransaction(tx);
+
+  const baseGas = 100000;
+  const options = {
+    // Required to short-circuit call to `estimateGasForTransactionExecution`
+    // - Tries to estimate gas using the `gasPrice` set in the safe tx
+    // ^- The `gasPrice` in the safe tx is also used for reimbursement
+    // ^- This creates a conflict where `gasPrice` must be both zero and non-zero
+    gasLimit: safeTx.data["safeTxGas"] + baseGas,
+  };
+
+  const executedTx = await safe.executeTransaction(safeTx, options);
+  const receipt = await executedTx.transactionResponse?.wait();
+
+  return receipt;
 }
 
 // Entrypoint for the Autotask
