02107247a668df497d845631dd12cf481b325e40
opz <will.shahda@gmail.com>
2022-08-13 22:30:25 -0400

    Update deploy script for multiple autotasks

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv))
+  .command("autotask", "The autotask script name")
+  .demandCommand().argv;
+
+const path = require("path");
 
 const { AutotaskClient } = require("defender-autotask-client");
 
-const main = async () => {
+const main = async (autotask) => {
   const {
     API_KEY: apiKey,
     API_SECRET: apiSecret,
@@ -10,11 +17,17 @@ const main = async () => {
   } = process.env;
   const client = new AutotaskClient({ apiKey, apiSecret });
 
-  await client.updateCodeFromFolder(autotaskId, "./src");
+  const autotaskPath = path.join("src", "autotasks", autotask);
+
+  try {
+    await client.updateCodeFromFolder(autotaskId, autotaskPath);
+  } catch {
+    throw new Error(`Invalid autotask name: ${autotask}`);
+  }
 };
 
 if (!module.parent) {
-  main()
+  main(argv._?.[0])
     .then(() => {
       process.exit(0);
     })
