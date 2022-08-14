d204f8f81f026629bd3100e7327b4af3a23de15a
opz <will.shahda@gmail.com>
2022-08-14 19:00:16 -0400

    Autotask sentinel to watch for mUSD depeg
    Uses coingecko for price data.

---
+++ src/autotasks/coingecko-depeg-sentinel/index.js
@@ -0,0 +1,47 @@
+const { ethers } = require("ethers");
+const coingecko = require("../../common/coingecko");
+const { DEPEG_THRESHOLDS, MUSD_ADDRESS } = require("../../common/constants");
+
+exports.main = async (events) => {
+  const matches = [];
+
+  const price = await coingecko.getTokenPrice(MUSD_ADDRESS);
+
+  if (price < 0) {
+    throw new RangeError("Token price cannot be negative");
+  }
+
+  if (price < DEPEG_THRESHOLDS[MUSD_ADDRESS]) {
+    for (const event of events) {
+      matches.push({ hash: event.hash });
+    }
+
+    return matches;
+  } else {
+    return [];
+  }
+};
+
+// Entrypoint for the Autotask
+exports.handler = async (payload) => {
+  const conditionRequest = payload.request.body;
+  const events = conditionRequest.events;
+
+  const matches = main(events);
+
+  return { matches };
+};
+
+// To run locally (this code will not be executed in Autotasks)
+if (require.main === module) {
+  const fs = require("fs");
+  const payload = fs.readFileSync(0, "utf-8");
+
+  exports
+    .handler(payload) // pass in JSON from piped stdin
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
