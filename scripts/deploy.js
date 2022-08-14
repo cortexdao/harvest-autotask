3d928e4a72ba65376befd8f1266f693576f0786c
opz <will.shahda@gmail.com>
2022-08-14 13:34:48 -0400

    Use rollups for autotask deployments

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,23 +1,49 @@
 require("dotenv").config();
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
+
+const deployBuilder = (yargs) => {
+  yargs
+    .positional("autotask", {
+      describe: "Autotask module name",
+      type: "string",
+    })
+    .positional("id", {
+      describe: "Autotask ID",
+      type: "string",
+      default: process.env.AUTOTASK_ID,
+    });
+};
+
+const deployHandler = (argv) => {
+  if (argv.id === undefined) {
+    throw new Error(
+      "Autotask ID must be specified in the CLI or in a $AUTOTASK_ID environment variable"
+    );
+  }
+};
+
 const argv = yargs(hideBin(process.argv))
-  .command("autotask", "The autotask script name")
+  .command(
+    "$0 <autotask> [id]",
+    "Deploy an Autotask",
+    deployBuilder,
+    deployHandler
+  )
   .demandCommand().argv;
 
 const path = require("path");
 
 const { AutotaskClient } = require("defender-autotask-client");
 
-const main = async (autotask) => {
-  const {
-    API_KEY: apiKey,
-    API_SECRET: apiSecret,
-    AUTOTASK_ID: autotaskId,
-  } = process.env;
+const main = async (argv) => {
+  const { API_KEY: apiKey, API_SECRET: apiSecret } = process.env;
+
   const client = new AutotaskClient({ apiKey, apiSecret });
 
-  const autotaskPath = path.join("src", "autotasks", autotask);
+  const { autotask, id: autotaskId } = argv;
+
+  const autotaskPath = path.join("build", "autotasks", autotask);
 
   try {
     await client.updateCodeFromFolder(autotaskId, autotaskPath);
@@ -27,7 +53,7 @@ const main = async (autotask) => {
 };
 
 if (!module.parent) {
-  main(argv._?.[0])
+  main(argv)
     .then(() => {
       process.exit(0);
     })
