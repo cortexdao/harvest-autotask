14510515ce93d0614d61ed3c44ef5eb215982394
opz <will.shahda@gmail.com>
2022-08-17 02:54:08 -0400

    Select the next index position for deposit
    Gets the position with the largest delta from the target value that
    accepts deposits with the token from the reserve pool with most excess
    value.

--- src/autotasks/rebalance-reserves/index.js
+++ src/autotasks/rebalance-reserves/index.js
@@ -4,23 +4,94 @@ const {
   DefenderRelayProvider,
 } = require("defender-relay-client/lib/ethers");
 
-const { TARGET_WEIGHTS, WEIGHT_DECIMALS } = require("../../common/constants");
 
-exports.getPositionDeltas = (positionSizes) => {
-  const nav = positionSizes.reduce((nav, { value }) => (nav += value), 0n);
+const {
+  TARGET_WEIGHTS,
+  WEIGHT_DECIMALS,
+  RESERVE_POOLS,
+} = require("../../common/constants");
+
+exports.getNav = (positions) => {
+  const nav = positions.reduce((nav, { value }) => (nav += value), 0n);
+  return nav;
+};
+
+exports.getTargetValues = (positions) => {
+  const nav = exports.getNav(positions);
 
   const targetValueEntries = TARGET_WEIGHTS.map(({ name, weight }) => {
     const value = (weight * nav) / 10n ** WEIGHT_DECIMALS;
     return [name, value];
   });
-  const targetValues = Object.fromEntries(targetValueEntries);
 
-  const sizeDeltas = positionSizes.map(({ name, value }) => {
+  const targetValues = Object.fromEntries(targetValueEntries);
+  return targetValues;
+};
+
+exports.getPositionDeltas = (positions) => {
+  const targetValues = exports.getTargetValues(positions);
+
+  const positionDeltas = positions.map(({ name, value }) => {
     const target = targetValues[name] || 0n;
     return { name, delta: target - value };
   });
 
-  return sizeDeltas;
+  return positionDeltas;
+};
+
+// Assumes amounts are normalized
+exports.sortRebalanceAmounts = (rebalanceAmounts) => {
+  // Negative amount means an excess of reserves
+  const sortAmounts = ({ amount: amountA }, { amount: amountB }) =>
+    amountA < amountB ? -1 : amountA > amountB ? 1 : 0;
+
+  // Variable for clarity, normalizedAmounts is sorted in place
+  const sortedAmounts = rebalanceAmounts.sort(sortAmounts);
+  return sortedAmounts;
+};
+
+exports.filterPositions = (underlyer, tokens) =>
+  tokens.map(({ token }) => token).includes(underlyer);
+
+exports.getRebalancePositions = (reserveAddress, positions) => {
+  const { underlyer } = RESERVE_POOLS[reserveAddress];
+  const filteredPositions = positions.filter(({ tokens }) =>
+    exports.filterPositions(underlyer, tokens)
+  );
+  return { address: reserveAddress, positions: filteredPositions };
+};
+
+exports.getAllRebalancePositions = (rebalanceAmounts, positions) => {
+  const rebalancePositions = rebalanceAmounts
+    .map(({ address }) => exports.getRebalancePositions(address, positions))
+    .filter(({ positions }) => positions.length > 0);
+
+  return rebalancePositions;
+};
+
+// Assumes amounts are normalized
+exports.getLargestRebalancePositions = (rebalanceAmounts, positions) => {
+  const sortedAmounts = exports.sortRebalanceAmounts(rebalanceAmounts);
+
+  const rebalancePositions = exports.getAllRebalancePositions(
+    sortedAmounts,
+    positions
+  );
+
+  const largestRebalancePositions = rebalancePositions.shift();
+
+  return largestRebalancePositions;
+};
+
+exports.getLargestPositionDelta = (positions) => {
+  const positionDeltas = exports.getPositionDeltas(positions);
+
+  const getLargerDelta = (largest, position) =>
+    position.delta > largest.delta ? position : largest;
+
+  const largestPositionDelta = positionDeltas.reduce(getLargerDelta);
+
+  return largestPositionDelta;
 };
 
 exports.main = async (signer) => {
