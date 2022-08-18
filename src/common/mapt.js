534392fe6bc21bcb728944632a6d610ea750b78b
opz <will.shahda@gmail.com>
2022-08-17 23:32:44 -0400

    Class to interact with mapt contract
    Repo is getting complex enough to warrant OOP

--- src/common/mapt.js
+++ src/common/mapt.js
@@ -5,23 +5,49 @@ const {
   RESERVE_POOLS,
   RESERVE_POOL_IDS,
 } = require("./constants");
+
 const maptAbi = require("../abis/MetaPoolTokenV2.json");
 
-/**
- * Output format:
- *
- *  [
- *    {
- *      address: "0x0", // Reserve pool address
- *      amount: -10n // Reserve pool rebalance amount
- *    },
- *    { address: "0x1", amount: 10n },
- *    { address: "0x2", amount: -20n },
- *  ]
- *
- * (-) amount means extra reserves that can fund the LP Account
- * (+) amount means reserves must be withdrawn from the LP Account
- */
+exports.MetaPoolToken = class {
+  constructor(signer) {
+    this.contract = new ethers.Contract(
+      META_POOL_TOKEN_ADDRESS,
+      maptAbi,
+      signer
+    );
+  }
+
+  /**
+   * Output format:
+   *
+   *  [
+   *    {
+   *      address: "0x0", // Reserve pool address
+   *      amount: -10n // Reserve pool rebalance amount
+   *    },
+   *    { address: "0x1", amount: 10n },
+   *    { address: "0x2", amount: -20n },
+   *  ]
+   *
+   * (-) amount means extra reserves that can fund the LP Account
+   * (+) amount means reserves must be withdrawn from the LP Account
+   */
+  async getRebalanceAmounts() {
+    const result = await this.contract.getRebalanceAmounts(RESERVE_POOL_IDS);
+
+    const rebalanceAmounts = result[0].map((address, i) => {
+      return { address, amount: result[1][i].toBigInt() };
+    });
+
+    return rebalanceAmounts;
+  }
+
+  async createFundLpAccountTx(reserveIds) {
+    const tx = createTx(this.contract, "fundLpAccount", [reserveIds]);
+    return tx;
+  }
+};
+
 exports.getRebalanceAmounts = async (signer) => {
   const mapt = new ethers.Contract(META_POOL_TOKEN_ADDRESS, maptAbi, signer);
   const result = await mapt.getRebalanceAmounts(RESERVE_POOL_IDS);
