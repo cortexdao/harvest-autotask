e1d18b7567e752cde1079402bca31e45e2a87674
opz <will.shahda@gmail.com>
2022-08-18 16:55:53 -0400

    Boilerplate for autotask that adds liquidity
    Will select the index position to add to and execute the transaction to
    add the liquidity.

---
+++ src/autotasks/add-liquidity/index.js
@@ -0,0 +1,31 @@
+const { ethers } = require("ethers");
+const {
+  DefenderRelaySigner,
+  DefenderRelayProvider,
+} = require("defender-relay-client/lib/ethers");
+
+exports.main = async (signer) => {};
+
+// Entrypoint for the Autotask
+exports.handler = async (credentials) => {
+  const provider = new DefenderRelayProvider(credentials);
+  const signer = new DefenderRelaySigner(credentials, provider, {
+    speed: "fast",
+  });
+  const receipt = exports.main(signer);
+
+  return receipt;
+};
+
+// To run locally (this code will not be executed in Autotasks)
+if (require.main === module) {
+  require("dotenv").config();
+  const { RELAY_API_KEY: apiKey, RELAY_API_SECRET: apiSecret } = process.env;
+  exports
+    .handler({ apiKey, apiSecret })
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
