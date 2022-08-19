cf4398bf731be4d05d0a6f3071d6b750b1735d27
opz <will.shahda@gmail.com>
2022-08-19 02:36:10 -0400

    Position vales and targets w/ the TvlManager class

--- src/common/tvlmanager.js
+++ src/common/tvlmanager.js
@@ -4,98 +4,139 @@ const {
   TVL_MANAGER_ADDRESS,
   LP_ACCOUNT_ADDRESS,
   USD_DECIMALS,
+  TARGET_WEIGHTS,
+  WEIGHT_DECIMALS,
 } = require("./constants");
 
 const tvlManagerAbi = require("../abis/TvlManager.json");
 const allocationAbi = require("../abis/IAssetAllocation.json");
 
-exports.getAllocations = async (allocationNames, tvlManager, signer) => {
-  const getAllocationAddress = (name) => tvlManager.getAssetAllocation(name);
-  const allocationAddressResults = await Promise.all(
-    allocationNames.map(getAllocationAddress)
-  );
-  const allocationAddresses = allocationAddressResults.filter(
-    (address) => address !== ethers.constants.AddressZero
-  );
+exports.TvlManager = class {
+  constructor(signer) {
+    this.signer = signer;
+    this.contract = new ethers.Contract(
+      TVL_MANAGER_ADDRESS,
+      tvlManagerAbi,
+      signer
+    );
+  }
 
-  const getAllocation = (address) =>
-    new ethers.Contract(address, allocationAbi, signer);
-  const allocations = allocationAddresses.map(getAllocation);
+  async getAllocations(allocationNames) {
+    const getAllocationAddress = (name) =>
+      this.contract.getAssetAllocation(name);
 
-  return allocations;
-};
+    const allocationAddressResults = await Promise.all(
+      allocationNames.map(getAllocationAddress)
+    );
 
-exports.getAllocationTokenPrices = async (tokens) => {
-  const uniqueTokenAddresses = [
-    ...new Set(tokens.flat().map(({ token }) => token)),
-  ];
+    const isAddressZero = (address) => address !== ethers.constants.AddressZero;
+    const allocationAddresses = allocationAddressResults.filter(isAddressZero);
 
-  const tokenPriceResults = await coingecko.getTokenPrice(uniqueTokenAddresses);
-  const tokenPriceEntries = uniqueTokenAddresses.map((address, i) => [
-    address,
-    tokenPriceResults[i],
-  ]);
-  const tokenPrices = Object.fromEntries(tokenPriceEntries);
+    const getAllocation = (address) =>
+      new ethers.Contract(address, allocationAbi, this.signer);
+    const allocations = allocationAddresses.map(getAllocation);
 
-  return tokenPrices;
-};
+    return allocations;
+  }
 
-exports.getTokenValue = async (
-  tokenPrices,
-  allocation,
-  { token, decimals },
-  i
-) => {
-  const balance = (
-    await allocation.balanceOf(LP_ACCOUNT_ADDRESS, i)
-  ).toBigInt();
-  const normalizedBalance =
-    (balance * 10n ** USD_DECIMALS) / 10n ** BigInt(decimals);
+  async getAllocationTokenPrices(tokens) {
+    const uniqueTokenAddresses = [
+      ...new Set(tokens.flat().map(({ token }) => token)),
+    ];
 
-  const tokenValue = coingecko.getUsdValueUnnormalized(
-    normalizedBalance,
-    tokenPrices[token]
-  );
+    const tokenPriceResults = await coingecko.getTokenPrice(
+      uniqueTokenAddresses
+    );
 
-  return tokenValue;
-};
+    const getTokenPriceEntry = (address, i) => [address, tokenPriceResults[i]];
+    const tokenPriceEntries = uniqueTokenAddresses.map(getTokenPriceEntry);
 
-exports.getValue = async (tokens, tokenPrices, allocation, i) => {
-  const tokenValues = await Promise.all(
-    tokens[i].map((token, i) =>
-      exports.getTokenValue(tokenPrices, allocation, token, i)
-    )
-  );
-  const value = tokenValues.reduce((a, b) => a + b);
+    const tokenPrices = Object.fromEntries(tokenPriceEntries);
 
-  const allocationName = await allocation.NAME();
+    return tokenPrices;
+  }
 
-  return { name: allocationName, value, tokens: tokens[i] };
-};
+  async getTokenValue(tokenPrices, allocation, { token, decimals }, i) {
+    const balance = (
+      await allocation.balanceOf(LP_ACCOUNT_ADDRESS, i)
+    ).toBigInt();
 
-exports.getIndexPositions = async (signer, allocationNames) => {
-  const tvlManager = new ethers.Contract(
-    TVL_MANAGER_ADDRESS,
-    tvlManagerAbi,
-    signer
-  );
+    const normalizedBalance =
+      (balance * 10n ** USD_DECIMALS) / 10n ** BigInt(decimals);
 
-  const allocations = await exports.getAllocations(
-    allocationNames,
-    tvlManager,
-    signer
-  );
+    const tokenValue = coingecko.getUsdValueUnnormalized(
+      normalizedBalance,
+      tokenPrices[token]
+    );
 
-  const getTokens = (allocation) => allocation.tokens();
-  const tokens = await Promise.all(allocations.map(getTokens));
+    return tokenValue;
+  }
 
-  const tokenPrices = await exports.getAllocationTokenPrices(tokens);
+  async getValue(tokens, tokenPrices, allocation, i) {
+    const tokenValuePromises = tokens[i].map((token, i) =>
+      this.getTokenValue(tokenPrices, allocation, token, i)
+    );
 
-  const positions = await Promise.all(
-    allocations.map((allocation, i) =>
-      exports.getValue(tokens, tokenPrices, allocation, i)
-    )
-  );
+    const tokenValues = await Promise.all(tokenValuePromises);
 
-  return positions;
+    const value = tokenValues.reduce((a, b) => a + b);
+    const allocationName = await allocation.NAME();
+
+    return { name: allocationName, value, tokens: tokens[i] };
+  }
+
+  async getIndexPositions(allocationNames) {
+    const allocations = await this.getAllocations(allocationNames);
+
+    const getTokens = (allocation) => allocation.tokens();
+    const tokens = await Promise.all(allocations.map(getTokens));
+
+    const tokenPrices = await this.getAllocationTokenPrices(tokens);
+
+    const positionPromises = allocations.map((allocation, i) =>
+      this.getValue(tokens, tokenPrices, allocation, i)
+    );
+    const positions = await Promise.all(positionPromises);
+
+    return positions;
+  }
+
+  getNav(positions) {
+    const nav = positions.reduce((nav, { value }) => (nav += value), 0n);
+    return nav;
+  }
+
+  getTargetValues(positions) {
+    const nav = this.getNav(positions);
+
+    const targetValueEntries = TARGET_WEIGHTS.map(({ name, weight }) => {
+      const value = (weight * nav) / 10n ** WEIGHT_DECIMALS;
+      return [name, value];
+    });
+
+    const targetValues = Object.fromEntries(targetValueEntries);
+    return targetValues;
+  }
+
+  getPositionDeltas(positions) {
+    const targetValues = this.getTargetValues(positions);
+
+    const positionDeltas = positions.map(({ name, value }) => {
+      const target = targetValues[name] || 0n;
+      return { name, delta: target - value };
+    });
+
+    return positionDeltas;
+  }
+
+  getLargestPositionDelta(positions) {
+    const positionDeltas = this.getPositionDeltas(positions);
+
+    const getLargerDelta = (largest, position) =>
+      position.delta > largest.delta ? position : largest;
+
+    const largestPositionDelta = positionDeltas.reduce(getLargerDelta);
+
+    return largestPositionDelta;
+  }
 };
