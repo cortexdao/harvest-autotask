cdb56efbb33f262e0e7f7a4c5f236e8b18c1fe22
opz <will.shahda@gmail.com>
2022-08-22 19:52:59 -0400

    Test normalization of token amounts

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,7 +1,15 @@
 const { expect } = require("chai");
-const { createTx } = require("../src/common/utils");
+const {
+  createTx,
+  convertTokenAmountDecimals,
+  normalizeTokenAmounts,
+} = require("../src/common/utils");
 
-const { USDC_ADDRESS } = require("../src/common/constants");
+const {
+  DAI_ADDRESS,
+  USDC_ADDRESS,
+  USDT_ADDRESS,
+} = require("../src/common/constants");
 
 const erc20Abi = require("../src/abis/ERC20.json");
 
@@ -17,6 +25,50 @@ describe("Utilities", () => {
       const erc20 = new ethers.Contract(USDC_ADDRESS, erc20Abi, signer);
       const tx = createTx(erc20, "transfer", [USDC_ADDRESS, 100n * 10n ** 6n]);
       expect(tx).to.include.all.keys("to", "data", "value");
+    });
+  });
+
+  describe("convertTokenAmountDecimals", () => {
+    it("should convert a token amount's decimals to the supplied decimals", async () => {
+      const tokenAmount = {
+        address: USDC_ADDRESS,
+        amount: 100n * 10n ** 6n,
+      };
+      const decimals = 18n;
+      const convertedTokenAmount = await convertTokenAmountDecimals(
+        tokenAmount,
+        decimals,
+        signer
+      );
+
+      const expectedTokenAmount = {
+        address: USDC_ADDRESS,
+        amount: 100n * 10n ** decimals,
+      };
+      expect(convertedTokenAmount).to.deep.equal(expectedTokenAmount);
+    });
+  });
+
+  describe("normalizeTokenAmounts", () => {
+    it("should return the results of convertTokenAmountDecimals for each token amount", async () => {
+      const tokenAmounts = [
+        { address: DAI_ADDRESS, amount: 100n * 10n ** 18n },
+        { address: USDC_ADDRESS, amount: 100n * 10n ** 6n },
+        { address: USDT_ADDRESS, amount: 100n * 10n ** 6n },
+      ];
+      const decimals = 18n;
+
+      const normalizedAmounts = normalizeTokenAmounts(
+        tokenAmounts,
+        decimals,
+        signer
+      );
+
+      const expectedTokenAmounts = [
+        { address: DAI_ADDRESS, amount: 100n * 10n ** decimals },
+        { address: USDC_ADDRESS, amount: 100n * 10n ** decimals },
+        { address: USDT_ADDRESS, amount: 100n * 10n ** decimals },
+      ];
     });
   });
 });
