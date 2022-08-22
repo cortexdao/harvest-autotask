b42af9659865123d3b7fa190549fa6faf745ac28
opz <will.shahda@gmail.com>
2022-08-22 17:29:29 -0400

    Test main and handler for add-liquidity autotask

---
+++ test/add-liquidity.test.js
@@ -0,0 +1,104 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+const {
+  impersonateAccount,
+  stopImpersonatingAccount,
+  setBalance,
+  takeSnapshot,
+} = require("@nomicfoundation/hardhat-network-helpers");
+
+const index = require("../src/autotasks/add-liquidity/index");
+const { main, handler } = index;
+const { LpAccount } = require("../src/common/lpaccount");
+
+const { addOwnerWithThreshold, forceTransfer } = require("./utils");
+const {
+  ADMIN_SAFE_ADDRESS,
+  THREEPOOL_STABLESWAP_ADDRESS,
+  LP_ACCOUNT_ADDRESS,
+  USDC_ADDRESS,
+  RESERVE_POOLS,
+} = require("../src/common/constants");
+
+const erc20Abi = require("../src/abis/ERC20.json");
+const poolTokenAbi = require("../src/abis/PoolTokenV3.json");
+
+describe("Add liquidity to index", () => {
+  let snapshot;
+  let signer;
+
+  beforeEach(async () => {
+    snapshot = await takeSnapshot();
+  });
+
+  beforeEach(async () => {
+    await addOwnerWithThreshold(signer.address);
+  });
+
+  beforeEach(async () => {
+    await impersonateAccount(ADMIN_SAFE_ADDRESS);
+    await setBalance(ADMIN_SAFE_ADDRESS, 10n ** 18n);
+    const adminSafeSigner = await ethers.getSigner(ADMIN_SAFE_ADDRESS);
+
+    const reserveAddresses = Object.keys(RESERVE_POOLS);
+    const getReserve = (address) =>
+      new ethers.Contract(address, poolTokenAbi, adminSafeSigner);
+    const reserves = reserveAddresses.map(getReserve);
+
+    const setReservePct = (reserve) => reserve.setReservePercentage(5);
+    const setReservePctPromises = reserves.map(setReservePct);
+
+    await Promise.all(setReservePctPromises);
+
+    await stopImpersonatingAccount(ADMIN_SAFE_ADDRESS);
+  });
+
+  beforeEach(async () => {
+    const usdc = new ethers.Contract(USDC_ADDRESS, erc20Abi, signer);
+    const extraUsdcBalance = 100000n * 10n ** 6n;
+    await forceTransfer(
+      usdc,
+      THREEPOOL_STABLESWAP_ADDRESS,
+      LP_ACCOUNT_ADDRESS,
+      extraUsdcBalance
+    );
+  });
+
+  afterEach(async () => {
+    await snapshot.restore();
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  before(async () => {
+    [signer] = await ethers.getSigners();
+  });
+
+  describe("main", () => {
+    it("should return a tx receipt", async () => {
+      // Drop a bunch of tokens in the LpAccount and set the reserve size
+      const receipt = await main(signer);
+      expect(receipt).to.include.all.keys("to", "from", "transactionHash");
+    });
+  });
+
+  const { RELAY_API_KEY: apiKey, RELAY_API_SECRET: apiSecret } = process.env;
+  if (apiKey && apiSecret) {
+    describe("handler", () => {
+      it("should return a tx receipt from main", async () => {
+        const expectedReceipt = {
+          to: ethers.constants.AddressZero,
+          from: ethers.constants.AddressZero,
+          transactionHash: "0x0",
+        };
+        sinon.replace(index, "main", () => Promise.resolve(expectedReceipt));
+
+        const receipt = await handler({ apiKey, apiSecret });
+
+        expect(receipt).to.deep.equal(expectedReceipt);
+      });
+    });
+  }
+});
